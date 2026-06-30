486c0acc15ddec5128120f4f6a32f16dd94c81f3
Dmitry <62464184+foxemperor@users.noreply.github.com>
2026-06-30 23:01:06 +0300

    Revert invalid charset config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/orokinbet-tool/',
-    build: {
-    charset: 'utf8',
-  },
-})
+  base: '/orokinbet-tool/
+    })
